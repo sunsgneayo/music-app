441635a2ba4edb47934cc4feefafa5cb02c1bea7
sunsgne <583985775@qq.com>
2024-12-19 15:33:11 +0800

    add music

--- public/songList.ts
+++ public/songList.ts
@@ -1,9 +1,65 @@
 const base = '.'
 export const songList = [
     {
+        artists: '刘若英',
+        name: '当爱在靠近',
+        avatar: 'https://p3.music.126.net/ZaPCRIUN5tnUoBzO8xlTvw==/109951169530667832.jpg?param=320y320',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E5%BD%93%E7%88%B1%E5%9C%A8%E9%9D%A0%E8%BF%91.flac',
+        lyric: base + '/LRC/当爱在靠近.lrc'
+    },
+    {
+        artists: '刘若英',
+        name: '后来',
+        avatar: 'https://p3.music.126.net/eBF7bHnJYBUfOFrJ_7SUfw==/109951163351825356.jpg?param=320y320',
+        link: "https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E5%90%8E%E6%9D%A5.flac",
+        lyric: base + '/LRC/后来.lrc'
+    },
+    {
+        artists: '伍佰',
+        name: '与你到永久',
+        avatar: 'https://p3.music.126.net/ZaPCRIUN5tnUoBzO8xlTvw==/109951169530667832.jpg?param=320y320',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E4%B8%8E%E4%BD%A0%E5%88%B0%E6%B0%B8%E4%B9%85.flac',
+        lyric: base + '/LRC/与你到永久.lrc'
+    },
+    {
+        artists: '刘德华',
+        name: '世界第一等',
+        avatar: 'https://p3.music.126.net/lI759NPPoU3WoRTu73R76g==/109951165994344537.jpg?param=320y320',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E4%B8%96%E7%95%8C%E7%AC%AC%E4%B8%80%E7%AD%89.flac',
+        lyric: base + '/LRC/世界第一等.lrc'
+    },
+    {
+        artists: '伍佰',
+        name: '再度重相逢',
+        avatar: 'https://p3.music.126.net/fQOh1ut3heT69l8IQF6ZBg==/109951164811428161.jpg?param=320y320',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E5%86%8D%E5%BA%A6%E9%87%8D%E7%9B%B8%E9%80%A2.flac',
+        lyric: base + '/LRC/再度重相逢.lrc'
+    },
+    {
+        artists: '孙燕姿',
+        name: '开始懂了',
+        avatar: 'https://p3.music.126.net/1HJILQwc62AfvcXkGgVJHQ==/109951165549820772.jpg?param=320y320',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E5%BC%80%E5%A7%8B%E6%87%82%E4%BA%86.flac',
+        lyric: base + '/LRC/开始懂了.lrc'
+    },
+    {
+        artists: '张韶涵',
+        name: '亲爱的，那不是爱情',
+        avatar: 'http://p2.music.126.net/Zo32hORqgy2prGj-OGGxjw==/109951170169025541.jpg?param=130y130',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E4%BA%B2%E7%88%B1%E7%9A%84%EF%BC%8C%E9%82%A3%E4%B8%8D%E6%98%AF%E7%88%B1%E6%83%85.flac',
+        lyric: base + '/LRC/亲爱的，那不是爱情.lrc'
+    },
+    {
+        artists: '伍佰',
+        name: '被动 (Live)',
+        avatar: 'http://p2.music.126.net/6DDZizgGgZv1VI4NkRGNAg==/109951166117582463.jpg',
+        link: 'https://facecontroller1.oss-cn-shanghai.aliyuncs.com/audio/MP3/%E8%A2%AB%E5%8A%A8%20feat.%20%E4%BC%8D%E4%BD%B0%28Live%29.flac',
+        lyric: base + '/LRC/被动 feat. 伍佰(Live).lrc'
+    },
+    {
         artists: '趙雷',
         name: '我記得',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/9bVOooAY6U6EJLzpv1Fikw==/109951169682871673.jpg?param=130y130',
         link: base + '/MP3/我記得.mp3',
         lyric: base + '/LRC/wjd.lrc'
     },
@@ -17,301 +73,301 @@ export const songList = [
     {
         artists: '趙雷',
         name: '程艾影',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/9bVOooAY6U6EJLzpv1Fikw==/109951169682871673.jpg?param=130y130',
         link: base +'/MP3/程艾影.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '岳云鹏',
         name: '西楼儿女',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/Ttltb_w0dpsPatb4boC6_A==/109951169486077212.jpg?param=130y130',
         link: base +'/MP3/岳云鹏-西楼儿女.mp3',
         lyric: base +'/LRC/西楼儿女y.lrc'
     },
     {
         artists: '岳云鹏',
         name: '问心无愧最自在',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/Ttltb_w0dpsPatb4boC6_A==/109951169486077212.jpg?param=130y130',
         link: base +'/MP3/问心无愧最自在.mp3',
         lyric: base +'/LRC/wxwkzzz.lrc'
     },
     {
         artists: '岳云鹏',
         name: '最亲的人',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/Ttltb_w0dpsPatb4boC6_A==/109951169486077212.jpg?param=130y130',
         link: base +'/MP3/最亲的人.mp3',
         lyric: base +'/LRC/zqdr.lrc'
     },
     {
         artists: '王菲',
         name: '红豆',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p2.music.126.net/WkzLUXccDmgD1vpEfVE-JA==/109951169790181226.jpg?param=130y130',
         link: base +'/MP3/红豆.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '王菲',
         name: '容易受伤的女人',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p2.music.126.net/4Ln6P1iMVWNyDeKgvVH_Cg==/109951169124297468.jpg?param=130y130',
         link: base +'/MP3/容易受伤的女人.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '王菲',
         name: '如愿',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p2.music.126.net/SvTfw8UsSPbt6bk-i0n3IQ==/109951166537609949.jpg?param=130y130',
         link: base +'/MP3/如愿.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '许茹芸',
         name: '独角戏',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/UsuyJt2QTcwXSeX9RjVc0g==/109951166782958283.jpg?param=130y130',
         link: base +'/MP3/独角戏.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '张杰',
         name: '天上掉下个林妹妹',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'http://p1.music.126.net/Uc2nXib0ql8FOvW2UGgq3Q==/109951166524231115.jpg?param=130y130',
         link: base +'/MP3/天上掉下个林妹妹.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '一路向北',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20160907/20160907190357712844.jpg',
         link: base +'/MP3/一路向北.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '七里香',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20210114/20210114010429615811.jpg',
         link: base +'/MP3/七里香.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '三年二班',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20230920/20230920142503632013.jpg',
         link: base +'/MP3/三年二班.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '不能说的秘密',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20201125/20201125100841821134.jpg',
         link: base +'/MP3/不能说的秘密.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '以父之名',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20230920/20230920142503632013.jpg',
         link: base +'/MP3/以父之名.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '兰亭序',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20241118/20241118160622508429.jpg',
         link: base +'/MP3/兰亭序.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '千里之外',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20170728/20170728122746411503.jpg',
         link: base +'/MP3/千里之外.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '半兽人',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220707/20220707130705658345.jpg',
         link: base +'/MP3/半兽人.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '双截棍',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220311/20220311161801880350.jpg',
         link: base +'/MP3/双截棍.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '反方向的钟',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20200620/20200620102018393175.jpg',
         link: base +'/MP3/反方向的钟.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '发如雪',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20200620/20200620104536553993.jpg',
         link: base +'/MP3/发如雪.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '听妈妈的话',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20170728/20170728122746411503.jpg',
         link: base +'/MP3/听妈妈的话.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '告白气球',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20200620/20200620103601113025.jpg',
         link: base +'/MP3/告白气球.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '夜曲',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20200620/20200620104536553993.jpg',
         link: base +'/MP3/夜曲.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '夜的第七章',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20170728/20170728122746411503.jpg',
         link: base +'/MP3/夜的第七章.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '忍者',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220311/20220311161801880350.jpg',
         link: base +'/MP3/忍者.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '明明就',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20231016/20231016111750787075.jpg',
         link: base +'/MP3/明明就.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '晴天',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20230920/20230920142503632013.jpg',
         link: base +'/MP3/晴天.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '最长的电影',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220705/20220705143006989099.jpg',
         link: base +'/MP3/最长的电影.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '本草纲目',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20170728/20170728122746411503.jpg',
         link: base +'/MP3/本草纲目.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '枫',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20200620/20200620104536553993.jpg',
         link: base +'/MP3/枫.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '烟花易冷',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20201125/20201125110726276231.jpg',
         link: base +'/MP3/烟花易冷.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '爱在西元前',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220311/20220311161801880350.jpg',
         link: base +'/MP3/爱在西元前.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '牛仔很忙',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220705/20220705143006989099.jpg',
         link: base +'/MP3/牛仔很忙.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '稻香',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20241118/20241118160622508429.jpg',
         link: base +'/MP3/稻香.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '等你下课',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220714/20220714213006413283.jpg',
         link: base +'/MP3/等你下课.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '给我一首歌的时间',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20241118/20241118160622508429.jpg',
         link: base +'/MP3/给我一首歌的时间.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '花海',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20241118/20241118160622508429.jpg',
         link: base +'/MP3/花海.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '菊花台',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20170728/20170728122746411503.jpg',
         link: base +'/MP3/菊花台.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '蒲公英的约定',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220705/20220705143006989099.jpg',
         link: base +'/MP3/蒲公英的约定.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '说好的幸福呢',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20241118/20241118160622508429.jpg',
         link: base +'/MP3/说好的幸福呢.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '霍元甲',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20200620/20200620103712669698.jpg',
         link: base +'/MP3/霍元甲.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '青花瓷',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220705/20220705143006989099.jpg',
         link: base +'/MP3/青花瓷.mp3',
         lyric: base +'/LRC/null.lrc'
     },
     {
         artists: '周杰伦',
         name: '龙拳',
-        avatar: 'https://spacexcode.oss-cn-hangzhou.aliyuncs.com/1697270523238-8b4b11a5-b5a3-4ac3-b6bd-1e264f526c76.png',
+        avatar: 'https://imgessl.kugou.com/stdmusic/20220707/20220707130705658345.jpg',
         link: base +'/MP3/龙拳.mp3',
         lyric: base +'/LRC/null.lrc'
     },
